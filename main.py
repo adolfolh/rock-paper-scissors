47ee86cde4a0e5b4a0726d53d0b9f58b81a846ab
Adolfo <psyal3@nottingham.ac.uk>
2020-09-10 20:04:03 +0200

    header label

--- main.py
+++ main.py
@@ -4,5 +4,14 @@ from tkinter import *
 window = Tk()
 window.title("Rock Paper Scissors by Adolfo López Herrera")
 
+# label header
+lbl = Label(window, text = "\nWelcome to Rock Paper Scissors!", font = ("Helvetica Bold",20))
+lbl.grid(column = 0, row = 0)
+lbl.config(anchor=CENTER)
+lbl.pack()
+
+# setting window size
+window.geometry('500x500')
+
 # run window
 window.mainloop()
